42ed6f66ac19999931c5aee8539a63d76429df53
David Diaz <ddiaz914@gmail.com>
2016-02-05 10:18:41 -0600

    Add structure to generate emails for when a user signs up for a class

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,19 @@ class UserMailer < ApplicationMailer
   default from: 'ddiaz914@gmail.com'
 
   def welcome_email(user)
+    attachments.inline['dgc-yoga-logo'] = { content: File.read("#{Rails.root}/app/assets/images/dbc-yoga-logo.png"),
+                           mime_type: "image/png" }
     @user = user
     @url  = 'http://localhost:3000/'
     mail(to: @user.email, subject: 'Welcome to DBC Yoga :D')
   end
+
+  def signup_email(user)
+    attachments.inline['dgc-yoga-logo'] = { content: File.read("#{Rails.root}/app/assets/images/dbc-yoga-logo.png"),
+                           mime_type: "image/png" }
+    @user = user
+    @url  = 'http://localhost:3000/'
+    mail(to: @user.email, subject: 'Welcome to DBC Yoga :D')
+  end
+
 end
